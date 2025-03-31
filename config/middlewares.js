c5f18846cb1aa508b9eb66bf5891f1f18cc2592e
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2025-03-31 01:54:18 -0300

    modifico politica de seguridad de cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,21 +7,28 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
+          'default-src': ["'self'", "data:", "blob:"],
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io'],
+          'img-src': ["'self'", 'data:', 'blob:', '*.cloudinary.com'],
           'media-src': [
             "'self'",
             'data:',
             'blob:',
             'market-assets.strapi.io',
-            'res.cloudinary.com',
+            '*.cloudinary.com',
           ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  'strapi::cors',
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
